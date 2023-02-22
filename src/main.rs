e81583101acc3e02b7949a6c941243bff330cc74
Wayne Duran <asartalo@gmail.com>
2023-02-22 22:48:01 +0800

    integration test and wip

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,71 @@
+use lightningcss::rules::font_face::{FontFaceRule, Source};
+use lightningcss::rules::CssRule::FontFace;
+use lightningcss::stylesheet::{ParserOptions, PrinterOptions, StyleSheet};
+use std::error::Error;
+use std::path::PathBuf;
+
 use clap::Parser;
 
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
 #[derive(Parser)]
-#[command(author = "Wayne Duran <asartalo@gmail.com>", version = "0.1.0", about = "Download fonts for self-hosting", long_about = None)]
+#[command(
+    author = "Wayne Duran <asartalo@gmail.com>",
+    version = "0.1.0",
+    about = "Download fonts for self-hosting",
+    long_about = None
+)]
 struct Cli {
     css_path: String,
+
+    #[arg(short = 'd', long = "dir", value_name = "DIRECTORY")]
+    dir: Option<PathBuf>,
+}
+
+fn get_font_url(rule: &FontFaceRule) -> Option<String> {
+    for property in rule.properties {
+        match property {
+            Source::Url(url_src) => return Some(url_src.url.url.to_string()),
+            _ => {
+                // do nothing
+            }
+        }
+    }
+    None
+}
+
+fn run() -> MyResult<()> {
+    let cli = Cli::parse();
+    let path = cli.css_path;
+
+    let response = minreq::get(path).send()?;
+    let stylesheet = StyleSheet::parse(response.as_str()?, ParserOptions::default()).unwrap();
+
+    let mut font_urls: Vec<String> = Vec::new();
+    for rule in &stylesheet.rules.0 {
+        match rule {
+            FontFace(ff_rule) => {
+                println!("Font Face: {:?}", ff_rule.properties);
+                match get_font_url(ff_rule) {
+                    None => {
+                        // do nothing
+                    }
+                    Some(url) => font_urls.push(url),
+                }
+            }
+            _ => {
+                // Do nothing.
+            }
+        }
+    }
+    let res = stylesheet.to_css(PrinterOptions::default()).unwrap();
+    println!("{}", res.code);
+    Ok(())
 }
 
 fn main() {
-    let cli = Cli::parse();
-    let path = cli.css_path;
-    println!("{}", path);
-    // let matches = Command::new("shfonts")
-    //     .version("0.1.0")
-    //     .author("Wayne Duran <asartalo@gmail.com>")
-    //     .about("Download fonts for self-hosting")
-    //     .arg(
-    //         Arg::new("cssPath")
-    //             .value_name("CSS_PATH")
-    //             .help("Path or URI to css file to source fonts")
-    //             .required(true)
-    //             .num_args(1)
-    //             .action(ArgAction::Set)
-    //     )
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
 }
--- tests/cli.rs
+++ tests/cli.rs
@@ -1,4 +1,7 @@
 use assert_cmd::Command;
+use httpmock::prelude::*;
+use std::fs;
+use std::path::Path;
 
 type TestResult = Result<(), Box<dyn std::error::Error>>;
 
@@ -8,5 +11,86 @@ fn dies_no_args() -> TestResult {
     cmd.assert()
         .failure()
         .stderr(predicates::str::contains("Usage"));
+    Ok(())
+}
+
+#[test]
+fn downloads_font_files() -> TestResult {
+    let server = MockServer::start();
+    let css_request_mock = server.mock(|when, then| {
+        when.method("GET")
+            .path("/css")
+            .query_param("family", "Roboto:300,300i,400");
+        then.status(200)
+            .body_from_file("tests/test_files/example.css");
+    });
+
+    let woff_i3x_mock = server.mock(|when, then| {
+        when.method("GET").path("/ri3lx.woff2");
+        then.status(200)
+            .body_from_file("tests/test_files/Roboto--italic--300--latin-ext.woff2");
+    });
+
+    let woff_i3_mock = server.mock(|when, then| {
+        when.method("GET").path("/ri3l.woff2");
+        then.status(200)
+            .body_from_file("tests/test_files/Roboto--italic--300--latin.woff2");
+    });
+
+    let woff_n3x_mock = server.mock(|when, then| {
+        when.method("GET").path("/rn3lx.woff2");
+        then.status(200)
+            .body_from_file("tests/test_files/Roboto--normal--300--latin-ext.woff2");
+    });
+
+    let woff_n3_mock = server.mock(|when, then| {
+        when.method("GET").path("/rn3l.woff2");
+        then.status(200)
+            .body_from_file("tests/test_files/Roboto--normal--300--latin.woff2");
+    });
+
+    let woff_n4x_mock = server.mock(|when, then| {
+        when.method("GET").path("/rn4lx.woff2");
+        then.status(200)
+            .body_from_file("tests/test_files/Roboto--normal--400--latin-ext.woff2");
+    });
+
+    let woff_n4_mock = server.mock(|when, then| {
+        when.method("GET").path("/rn4l.woff2");
+        then.status(200)
+            .body_from_file("tests/test_files/Roboto--normal--400--latin.woff2");
+    });
+
+    let css_url = server.url("/css?family=Roboto:300,300i,400");
+    let dir = Path::new("./tmp");
+
+    // Remove the output directory if it already exists
+    if dir.exists() {
+        fs::remove_dir_all(dir)?;
+    }
+
+    let mut cmd = Command::cargo_bin("shfonts")?;
+    cmd.arg(css_url)
+        .arg(format!("--dir={}", dir.to_str().unwrap()))
+        .assert()
+        .success();
+
+    // Check if server requests were called
+    css_request_mock.assert();
+    woff_i3x_mock.assert();
+    woff_i3_mock.assert();
+    woff_n3x_mock.assert();
+    woff_n3_mock.assert();
+    woff_n4x_mock.assert();
+    woff_n4_mock.assert();
+
+    // Check that the font files were downloaded correctly
+    assert!(dir.join("ri3lx.woff2").exists());
+    assert!(dir.join("ri3l.woff2").exists());
+    assert!(dir.join("rn3lx.woff2").exists());
+    assert!(dir.join("rn3l.woff2").exists());
+    assert!(dir.join("rn4lx.woff2").exists());
+    assert!(dir.join("rn4l.woff2").exists());
+
     Ok(())
 }
